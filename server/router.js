9cddd008228d58ff85d2c51ca723808467aef0b2
taranjeet singh <taranjeetplay@gmail.com>
2016-10-14 03:46:24 -0700

    token based authorization working fine

--- server/router.js
+++ server/router.js
@@ -1,4 +1,14 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+//create a middleware for passport
+const requireAuth = passport.authenticate('jwt',{ session : false });
+//here we are using a token based strategy
+
 module.exports= function(app) {
+    app.get('/',requireAuth,function(req,res) {
+        res.send({"hi":"there"});
+    });
     app.post('/signup',Authentication.signup);
 }
